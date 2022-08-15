9858232cd7cefd488a21f9a340ee2ccd0ed9b3ce
johan <stjernholm@msn.com>
2022-08-15 23:48:13 +0800

    Feature: settings directory for dev and prod

---
+++ composeexample/settings/dev.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Set environment variable:
+# export DJANGO_SETTINGS_MODULE="composeexample.settings.dev"
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-5-pwk@)k^nupu-0uw04_xx$*z^#0)%tbapx$3t!gab$!0*_(ta'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ composeexample/settings/prod.py
@@ -0,0 +1,20 @@
+import os
+from .base import *
+
+# Set environment variable:
+# export DJANGO_SETTINGS_MODULE="composeexample.settings.prod"
+
+# Deployment settings:
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Add webdomain or server IP
+ALLOWED_HOSTS = ["localhost"]
+
+# collect static files with command: 'python manage.py collectstatic'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
