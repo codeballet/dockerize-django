e9548e4d62db2dadd7e6618273fa429d4ebdf731
johan <stjernholm@msn.com>
2022-08-18 11:41:43 +0800

    Feature: add JavaScript to static

---
+++ tasks/static/tasks/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const h1 = document.createElement('h1');
+    h1.innerHTML = "Created in JavaScript!";
+    document.querySelector('body').append(h1);
+})
